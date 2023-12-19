3b1e472f2b973c4042db4d818625fe6af2bea93c
kamal sharma <kamalbsr2101@gmail.com>
2023-12-19 15:57:57 +0530

    I have created two files one of the product.js and one of updateProduct.js and some files modified

---
+++ frontend/src/pages/admin/Products.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/layout/Layout";
+import AdminMenu from "../../components/layout/AdminMenu";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  //   getAllProducts
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/product/get-product");
+      if (data?.success) {
+        setProducts(data?.product);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
+  return (
+    <Layout title={"Dashboard-Products"}>
+      <div className="row m-3 p-3">
+        <div className="col-md-3">
+          <AdminMenu />
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center">All Products List</h1>
+          <div className="d-flex flex-wrap justify-content-center">
+            {products?.map((pro) => (
+              <Link
+                key={pro._id}
+                to={`/dashboard/admin/product/${pro.slug}`}
+                className="product-link"
+              >
+                <div className="card m-2" style={{ width: "18rem" }}>
+                  <img
+                    src={`/api/product/product-photo/${pro._id}`}
+                    className="card-img-top"
+                    alt={pro.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{pro.name}</h5>
+                    <p className="card-text">{pro.description}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Products;
---
+++ frontend/src/pages/admin/UpdateProduct.js
@@ -0,0 +1,196 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/layout/Layout";
+import AdminMenu from "../../components/layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Select } from "antd";
+const { Option } = Select;
+
+const UpdateProduct = () => {
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [shipping, setShipping] = useState("");
+  const [photo, setPhoto] = useState("");
+
+  //   get single product
+  const getSingleProduct = async () => {
+    try {
+      const { data } = await axios.get(
+        `/api/product/get-product/:${params.slug}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // get all categories
+  const getAllCategories = async () => {
+    try {
+      const { data } = await axios.get("/api/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategories();
+  }, []);
+
+  // handle Create
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const productData = new FormData();
+      productData.append("name", name);
+      productData.append("description", description);
+      productData.append("price", price);
+      productData.append("quantity", quantity);
+      productData.append("photo", photo);
+      productData.append("category", category);
+
+      const { data } = await axios.put(
+        `/api/product/update-product/:${id}`,
+        productData
+      );
+      if (data?.success) {
+        toast.success(data?.message);
+        navigate("/dashboard/admin/products");
+      } else {
+        toast.error("Error in product created");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <Layout title={"Dashboard - Create Product"}>
+      <div className="container-fluid m-3 p-3 dashboard">
+        <div className="row">
+          <div className="col-md-3">{<AdminMenu />}</div>
+          <div className="col-md-9">
+            <h2>Update Product</h2>
+            <div className="m-1 w-75">
+              {/* <form onSubmit={handleCreate}> */}
+              <Select
+                bordered={false}
+                placeholder="Select a category"
+                size="large"
+                showSearch
+                className="form-select p-0 mb-3"
+                onChange={(value) => {
+                  setCategory(value);
+                }}
+              >
+                {categories?.map((c) => (
+                  <Option key={c._id} value={c._id}>
+                    {c.name}
+                  </Option>
+                ))}
+              </Select>
+              <div className="mb-3">
+                <label className="btn btn-outline-secondary col-md-12">
+                  {photo ? photo.name : "Upload photo"}
+                  <input
+                    type="file"
+                    name="photo"
+                    accept="image/*"
+                    onChange={(e) => {
+                      setPhoto(e.target.files[0]);
+                    }}
+                    hidden
+                  />
+                </label>
+              </div>
+              <div className="mb-3">
+                {photo && (
+                  <div className="text-center">
+                    <img
+                      src={URL.createObjectURL(photo)}
+                      alt="product photo"
+                      height={"200px"}
+                      className="img img-responsive"
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={name}
+                  placeholder="Write a name"
+                  className="form-control"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <textarea
+                  type="text"
+                  value={description}
+                  placeholder="Description"
+                  className="form-control"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={price}
+                  placeholder="Price"
+                  className="form-control"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={quantity}
+                  placeholder="Quantity"
+                  className="form-control"
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <Select
+                  bordered={false}
+                  size="large"
+                  value={shipping}
+                  placeholder="Select shipping"
+                  // showSearch
+                  className="form-select p-0"
+                  onChange={(value) => setShipping(value)}
+                >
+                  <Option value="0">Yes</Option>
+                  <Option value="1">No</Option>
+                </Select>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  onClick={handleUpdate}
+                >
+                  Update Product
+                </button>
+              </div>
+              {/* </form> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default UpdateProduct;
